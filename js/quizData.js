14ac15b0c0955ee00843d3e05b5c8de8fb36d99e
sawdahoque234 <sawdahoque6066@gmail.com>
2022-10-28 21:56:19 +0600

    ok

---
+++ js/quizData.js
@@ -0,0 +1,97 @@
+[
+  {
+    title: "What is the past form of 'eat'?",
+    options: ["eat", "ate", "eaten", "have ate"],
+    answer: "ate",
+  },
+  {
+    title: "Which sentence is present continuous tense?",
+    options: [
+      "I eat rice",
+      "I am eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I am eating rice",
+  },
+  {
+    title: "Which sentence is present perfect tense?",
+    options: [
+      "I eat rice",
+      "I am eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I have eaten rice",
+  },
+  {
+    title: "Which sentence is present perfect continuous tense?",
+    options: [
+      "I eat rice",
+      "I am eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I have been eating rice for 1 year",
+  },
+  {
+    title: "Which sentence is past continuous tense?",
+    options: [
+      "I eat rice",
+      "I was eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I was eating rice",
+  },
+  {
+    title: "Which sentence is past perfect tense?",
+    options: [
+      "I eat rice",
+      "I was eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I have eaten rice",
+  },
+  {
+    title: "Which sentence is past perfect continuous tense?",
+    options: [
+      "I eat rice",
+      "I was eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I have been eating rice for 1 year",
+  },
+  {
+    title: "Which sentence is future continuous tense?",
+    options: [
+      "I eat rice",
+      "I will be eating rice",
+      "I have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I will be eating rice",
+  },
+  {
+    title: "Which sentence is future perfect tense?",
+    options: [
+      "I eat rice",
+      "I will be eating rice",
+      "I will have eaten rice",
+      "I have been eating rice for 1 year",
+    ],
+    answer: "I will have eaten rice",
+  },
+  {
+    title: "Which sentence is future perfect continuous tense?",
+    options: [
+      "I eat rice",
+      "I will be eating rice",
+      "I will have eaten rice",
+      "I will have been eating rice for 1 year",
+    ],
+    answer: "I will have been eating rice for 1 year",
+  },
+];
